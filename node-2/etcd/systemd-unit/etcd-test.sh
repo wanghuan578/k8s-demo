a569e2a381837c4887902c3bc810a7f1872c9520
wanghuan666 <57810140@qq.com>
2018-02-28 16:27:53 +0800

    ...

--- node-2/etcd/systemd-unit/etcd-test.sh
+++ node-2/etcd/systemd-unit/etcd-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for ip in ${NODE_IPS}; do
-ETCDCTL_API=3 /root/local/bin/etcdctl \
+ETCDCTL_API=3 /usr/bin/etcdctl \
 --endpoints=https://${ip}:2379 \
 --cacert=/etc/kubernetes/ssl/ca.pem \
 --cert=/etc/etcd/ssl/etcd.pem \
