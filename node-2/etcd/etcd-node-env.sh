98319fc888d4cd8109c99dc658381a80ece6e4e2
wanghuan666 <57810140@qq.com>
2018-02-28 14:18:05 +0800

    change dir name

--- node-2/etcd/etcd-node-env.sh
+++ node-2/etcd/etcd-node-env.sh
@@ -11,6 +11,3 @@ export NODE_IPS="192.168.207.128 192.168.207.129 192.168.207.131"
 
 # etcd 集群间通信的IP和端口
 export ETCD_NODES=etcd-host0=https://192.168.207.128:2380,etcd-host1=https://192.168.207.129:2380,etcd-host2=https://192.168.207.131:2380
-
-# 导入用到的其它全局变量：ETCD_ENDPOINTS、FLANNEL_ETCD_PREFIX、CLUSTER_CIDR
-source /home/git/k8s-demo/environment.sh
