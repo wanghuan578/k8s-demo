797d05aefbe9d9403d6f1edd5889c7d22a6c04ed
wanghuan578 <57810140@qq.com>
2018-02-28 03:58:34 -0500

    .

--- auto/master-boot-strap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-systemctl daemon-reload
-systemctl enable kube-apiserver
-systemctl start kube-apiserver
-systemctl enable kube-controller-manager
-systemctl start kube-controller-manager
-systemctl enable kube-scheduler
-systemctl start kube-scheduler
