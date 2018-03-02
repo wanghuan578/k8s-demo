07cfbb0f5fc0ef8f16d0622e8a3604884216fa70
wanghuan578 <57810140@qq.com>
2018-03-01 22:11:53 -0500

    flanneld ca

---
+++ tls/flanneld/flanneld-ca-gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cfssl gencert -ca=/etc/kubernetes/ssl/ca.pem \
+-ca-key=/etc/kubernetes/ssl/ca-key.pem \
+-config=/etc/kubernetes/ssl/ca-config.json \
+-profile=kubernetes flanneld-csr.json | cfssljson -bare flanneld
+
+mkdir /etc/flanneld/ssl -p
+mv flanneld*.pem /etc/flanneld/ssl
+rm flanneld.csr flanneld-csr.json
---
+++ tls/flanneld/flanneld-csr-gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cat > flanneld-csr.json <<EOF
+{
+"CN": "flanneld",
+"hosts": [],
+"key": {
+"algo": "rsa",
+"size": 2048
+},
+"names": [
+{
+"C": "CN",
+"ST": "BeiJing",
+"L": "BeiJing",
+"O": "k8s",
+"OU": "System"
+}
+]
+}
+EOF
