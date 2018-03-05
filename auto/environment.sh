1400b7b1478e913379c31e28f0a8674ed6c253a7
wanghuan666 <57810140@qq.com>
2018-03-05 12:54:26 +0800

    export env

--- auto/environment.sh
+++ auto/environment.sh
@@ -1,31 +1,31 @@
 # TLS Bootstrapping 使用的 Token，可以使用命令 head -c 16 /dev/urandom | od -An -t x | tr -d ' ' 生成
-BOOTSTRAP_TOKEN="2b99f36be7124d6d37751c4aadf9f95d"
+export BOOTSTRAP_TOKEN="2b99f36be7124d6d37751c4aadf9f95d"
 
 # 建议用 未用的网段 来定义服务网段和 Pod 网段
 # 服务网段 (Service CIDR），部署前路由不可达，部署后集群内使用 IP:Port可达
-SERVICE_CIDR="10.254.0.0/16"
+export SERVICE_CIDR="10.254.0.0/16"
 
 # POD 网段 (Cluster CIDR），部署前路由不可达，**部署后**路由可达 (flanneld 保证)
-CLUSTER_CIDR="172.30.0.0/16"
+export CLUSTER_CIDR="172.30.0.0/16"
 
 # 服务端口范围 (NodePort Range)
-NODE_PORT_RANGE="8400-9000"
+export NODE_PORT_RANGE="8400-9000"
 
 # etcd 集群服务地址列表
-ETCD_ENDPOINTS="https://192.168.207.128:2379,https://192.168.207.129:2379,https://192.168.207.131:2379"
+export ETCD_ENDPOINTS="https://192.168.207.128:2379,https://192.168.207.129:2379,https://192.168.207.131:2379"
 
 # flanneld 网络配置前缀
-FLANNEL_ETCD_PREFIX="/kubernetes/network"
+export FLANNEL_ETCD_PREFIX="/kubernetes/network"
 
 # kubernetes 服务 IP (预分配，一般是 SERVICE_CIDR 中第一个IP)
-CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
+export CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
 
 # 集群 DNS 服务 IP (从 SERVICE_CIDR 中预分配)
-CLUSTER_DNS_SVC_IP="10.254.0.2"
+export CLUSTER_DNS_SVC_IP="10.254.0.2"
 
 # 集群 DNS 域名
-CLUSTER_DNS_DOMAIN="cluster.local."
+export CLUSTER_DNS_DOMAIN="cluster.local."
 
-MASTER_IP=192.168.207.128
+export MASTER_IP=192.168.207.128
 
-KUBE_APISERVER="https://${MASTER_IP}:6443"
+export KUBE_APISERVER="https://${MASTER_IP}:6443"
