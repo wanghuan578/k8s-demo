c00d64d2419a5c47b192f68d07000d1f59b19e26
wanghuan578 <57810140@qq.com>
2018-03-06 21:13:39 -0500

    fix docker systemd unit

--- node-1/docker/systemd-unit/docker-service-gen.sh
+++ node-1/docker/systemd-unit/docker-service-gen.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-cat > docker.service <<EOF
-[Unit]
-Description=Docker Application Container Engine
-Documentation=http://docs.docker.io
-[Service]
-Environment="PATH=/root/local/bin:/bin:/sbin:/usr/bin:/usr/sbin"
-EnvironmentFile=-/run/flannel/docker
-ExecStart=/usr/bin/dockerd --log-level=error $DOCKER_NETWORK_OPTIONS
-ExecReload=/bin/kill -s HUP $MAINPID
-Restart=on-failure
-RestartSec=5
-LimitNOFILE=infinity
-LimitNPROC=infinity
-LimitCORE=infinity
-Delegate=yes
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cp docker.service /etc/systemd/system/docker.service
+cp docker.service /etc/systemd/system
 systemctl daemon-reload
 systemctl stop firewalld
 systemctl disable firewalld
--- node-2/docker/docker-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-systemctl daemon-reload
-systemctl stop firewalld
-systemctl disable firewalld
-iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat
-systemctl enable docker
-systemctl start docker
